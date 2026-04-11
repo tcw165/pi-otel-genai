cb6d822464d71d987968cc8e90efdde17c36ae7e
TC Wang <boyw165@gmail.com>
2026-04-11 10:49:16 -0700

    Add turn logging

--- src/trace/session_node.ts
+++ src/trace/session_node.ts
@@ -5,11 +5,32 @@ export class SessionNode {
     readonly id: string,
     public parent: SessionNode | undefined = undefined,
     public children: SessionNode[] = [],
-    /** Active agent span */
-    public agentSpan: Span | undefined = undefined,
+
+    /**
+     * Active agent node.
+     * Note: A session could have many agents but can only have ONE acitve agent.
+     */
+    public agent: AgentNode | undefined = undefined,
   ) {}
 
   addChild(child: SessionNode): void {
     this.children.push(child);
   }
+}
+
+export class AgentNode {
+  constructor(
+    public agentSpan: Span | undefined,
+    /**
+     * An agent could have 1-to-many turns
+     */
+    public turnNodes: TurnNode[] = [],
+  ) {}
+}
+
+export class TurnNode {
+  constructor(
+    /** Active tool spans keyed by toolCallId */
+    public toolSpans: Map<string, Span> = new Map(),
+  ) {}
 }
